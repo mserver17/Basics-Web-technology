6801f41b48c9b2e48adf8c05c263e81c83eff4ef
Munar <m.suinalievich@gmail.com>
2024-02-07 14:48:33 +0300

    layout

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+var backToTopButton = document.querySelector('.link-nuv-up');
+
+var timer;
+function hideBackToTopButton() {
+    backToTopButton.style.bottom = '-50px';
+}
+window.addEventListener('scroll', function() {   
+    if (window.scrollY > 100) { 
+        backToTopButton.style.bottom = '20px'; 
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(hideBackToTopButton, 2000); 
+    } else {
+        backToTopButton.style.bottom = '-50px'; 
+    }
+});
+
+backToTopButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
 
