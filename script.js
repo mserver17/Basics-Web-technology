b901d68931782a43f3e3eec8fbf610885d9b5c8a
Munar <m.suinalievich@gmail.com>
2024-03-07 21:58:08 +0300

    exactly intermediate

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-var backToTopButton = document.querySelector('.link-nuv-up');
+const backToTopButton = document.querySelector('.link-nuv-up');
 
-var timer;
+let timer;
 function hideBackToTopButton() {
     backToTopButton.style.bottom = '-50px';
 }
-window.addEventListener('scroll', function() {   
-    if (window.scrollY > 100) { 
-        backToTopButton.style.bottom = '20px'; 
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) {
+        backToTopButton.style.bottom = '20px';
         if (timer) {
             clearTimeout(timer);
         }
-        timer = setTimeout(hideBackToTopButton, 2000); 
+        timer = setTimeout(hideBackToTopButton, 2000);
     } else {
-        backToTopButton.style.bottom = '-50px'; 
+        backToTopButton.style.bottom = '-50px';
     }
-}); 
+});
 
 backToTopButton.addEventListener('click', function(event) {
     event.preventDefault();
@@ -24,10 +25,32 @@ backToTopButton.addEventListener('click', function(event) {
     });
 });
 
-document.querySelector('.burger').addEventListener('click', function(){
+document.querySelector('.burger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.nav').classList.toggle('open');
-    
-})
+});
 
+document.addEventListener('DOMContentLoaded', function() {
+    const phoneInput = document.querySelector('.person-phone');
+    const emailInput = document.querySelector('.person-email');
 
+    phoneInput.addEventListener('input', function(event) {
+        const inputValue = event.target.value;
+        const onlyDigits = inputValue.replace(/\D/g, '');
+        event.target.value = onlyDigits;
+    });
+
+    emailInput.addEventListener('input', function(event) {
+        const inputValue = event.target.value;
+        if (!isValidEmail(inputValue)) {
+            event.target.setCustomValidity('Invalid email address');
+        } else {
+            event.target.setCustomValidity('');
+        }
+    });
+});
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
